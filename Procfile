82c4b1a6e8824adde14cfa29cdc12877579aafff
Christoph <rintzchristoph@gmail.com>
2026-04-23 17:45:59 +0200

    Configure project for production deployment on Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dbproject.wsgi:application --bind 0.0.0.0:$PORT
--- dbproject/settings.py
+++ dbproject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-aw2#38u2p6wr1tw5)#+dofdefukwxcx%m)mp1=fj=&f2#pp##i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
